6c3856ec799a88bcb159cf663b89a04c1a349a0a
Adrian Cardenas <SonHak@github.com>
2017-09-29 19:03:17 +0200

    afegit comecomentaris

--- calendari.php
+++ calendari.php
@@ -13,7 +13,6 @@ $diaSemana=date("w",mktime(0,0,0,$mes,1,$año));
 		table {margin: 0 auto;}
 	</style>
 	<body>
-		
 	<table>
 		<tr>
 			<?php  echo "<caption>    $mesLetra &nbsp;  $año</caption>" ?>;
@@ -24,20 +23,28 @@ $diaSemana=date("w",mktime(0,0,0,$mes,1,$año));
 			<?php
 				$dia = 1;
 				for($i=1;$i <=42;$i++){
+					//coloca el primer día en el día de la semana correspondiente
 					if ($i == $diaSemana)
 					{
 						echo "<td> $dia </td>";
 						$dia++;
 					}
+					
+					//si la variable i es más pequeña al dia de la semana (en este caso 5)
+					//o es más grande al total de días que tiene el més (más cuatro espacios en blanco que representarían los últimos días del mes anterior)
 					else if ($i < $diaSemana || $i > $totalDiasMes+4)
 					{
 						 echo "<td> &nbsp; </td>";
 					}
+					
+					// si la variable día es igual al día actual, lo pintará con otro color
 					else if ($dia == $diaActual)
 					{
 						echo "<td id='actual'>$dia</td>";
 						$dia++;
 					}
+					
+					// simplemente coloca los demás días
 					else 
 					{
 						echo "<td> $dia </td>";
